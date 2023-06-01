2ec96e8e03a7b8e6a3536080f54f9e84deaf0213
Mark Miranda <mmiranda@doximity.com>
2023-06-01 11:10:50 -0600

    Add token & expiration to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_01_165234) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_01_170816) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_01_165234) do
     t.string "nickname", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "token"
+    t.datetime "token_expiration"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
